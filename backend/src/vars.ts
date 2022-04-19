0858b1b3a02d5cffed33da3ae93de97ec661f26b
Leandro Santiago Gomes <leandroluk@gmail.com>
2022-04-19 12:02:37 -0300

    ajustando config's de backend

--- backend/src/vars.ts
+++ backend/src/vars.ts
@@ -7,8 +7,5 @@ export default {
   },
   mysql: {
     uri: env.MYSQL_URI || 'mysql://root:root@localhost:3306/db'
-  },
-  mongo: {
-    uri: env.MONGO_URI || 'mongodb://localhost:27017/db'
   }
 }
